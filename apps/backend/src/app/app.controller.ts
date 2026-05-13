0f63454e3ffd8717153a8b71c5e9c76df6dac30d
sacru2red <sacru2red@gmail.com>
2026-05-13 17:55:25 +0900

    chore: setup nestia

--- apps/backend/src/app/app.controller.ts
+++ apps/backend/src/app/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
+import { TypedParam, TypedRoute } from '@nestia/core';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @TypedRoute.Get()
   getData() {
     return this.appService.getData();
   }
+
+  @TypedRoute.Get('/:id')
+  checkIdIsNumber(@TypedParam('id') id: number) {
+    console.log('id', id)    
+  } 
 }
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,11 +6,18 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { NestiaSwaggerComposer } from '@nestia/sdk';
+import { SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  if (process.env.NODE_ENV !== 'production') {
+    const document = await NestiaSwaggerComposer.document(app, {});
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    SwaggerModule.setup('swagger-doc', app, document as any);
+  }
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
